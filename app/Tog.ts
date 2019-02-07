f5b884871462bc6297f98bdf0fc698f2d6bd0aa0
Tom Chandler <thomasjchandler@gmail.com>
2019-02-06 23:11:13 -0500

    Make Togs leap

--- app/Tog.ts
+++ app/Tog.ts
@@ -1,4 +1,4 @@
-import { WebGLRenderer, Graphics, Container } from "pixi.js";
+import { WebGLRenderer, Graphics, Container, Point } from "pixi.js";
 import * as _ from 'lodash';
 
 const WIDTH = 5;
@@ -23,6 +23,7 @@ export default class Tog {
     private skipChance: number = 0.75;
     private inclination: {x: number, y: number};
     private pep: number;
+    private soaring: { remainingX: number, remainingY: number, timeInFlight: number, timeRemaining: number };
 
     private jumpOffset = this.jumpPhase();
 
@@ -67,18 +68,30 @@ export default class Tog {
     }
 
     private createSprite(x: number, y: number) {
-        this.color = Math.floor(Math.random() * 0xFFFFFF);
+        this.color = _.random(0xFFFFFF, false);
         this.sprite = new Graphics();
         this.sprite.cacheAsBitmap = true;
         this.sprite.interactive = true;
         this.sprite.hitArea = new PIXI.Rectangle(0, 0, WIDTH * 1.2, HEIGHT * 1.2);
         this.sprite.on('mouseover', mouseData => {
-            this.teleport();
+            this.soar();
         });
         this.sprite.moveTo(x, y)
             .beginFill(this.color)
             .drawRect(0, 0, WIDTH, HEIGHT)
             .endFill();
+    }
+
+    private soar() {
+        if (this.soaring == null) {
+            const timeInFlight = _.random(30, 120, false)
+            this.soaring = {
+                remainingX:  (_.random(this.xWrap, false) - this.position.x) / timeInFlight,
+                remainingY: (_.random(this.yWrap, false) - this.position.y) / timeInFlight,
+                timeInFlight,
+                timeRemaining: timeInFlight
+            }
+        }
     }
 
     private teleport() {
@@ -90,6 +103,30 @@ export default class Tog {
 
     private getUpdatedPosition(mousePosition) {
         let { x, y } = this.position;
+
+        if (this.soaring) {
+            const soaringPosition = {
+                x: x + this.soaring.remainingX,
+                y: y + this.soaring.remainingY
+            }
+
+            const midPoint = this.soaring.timeInFlight / 2
+            const scale = midPoint > this.soaring.timeRemaining ?
+                (this.soaring.timeRemaining * 2) / this.soaring.timeInFlight :
+                (this.soaring.timeInFlight - this.soaring.timeRemaining) * 2 / this.soaring.timeInFlight
+
+            this.sprite.scale.x = 1 + scale * 1.5
+            this.sprite.scale.y =  1 + scale * 1.5
+
+            this.soaring.timeRemaining -= 1
+            if (this.soaring.timeRemaining === 0) {
+                this.soaring = null
+            }
+
+            return soaringPosition
+        } else {
+            this.sprite.scale = new Point(1, 1)
+        }
 
         let { x: dx, y: dy } =
             this.determineNewHeading(
@@ -175,7 +212,7 @@ export default class Tog {
     private reconsider() {
         const shouldReconsider = Math.random() < this.skipChance;
         if (shouldReconsider && !this.heading.chase) {
-            this.framesToSkip = 10 + Math.floor(Math.random() * 30);
+            this.framesToSkip = 10 + _.random(30, false);
             this.heading = getHeading();
         }
     }
